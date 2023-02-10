3d23a1fc4c2f88456f18663637a91df787fbd5ed
andmar680 <andmar680@gmail.com>
2023-02-10 13:13:03 -0600

    first attempt

--- python/palindrome.py
+++ python/palindrome.py
@@ -1,2 +1,26 @@
+import re
+    
 def palindrome(word):
-    # Write code here
+    
+    word = str(word).lower()
+    word = re.sub(r"\W", "", word)
+    # .isalnum()  can also determine if currentElem is alphanumeric and return True or False
+    # print(word)
+    
+    if word == word[::-1]:
+        return True
+
+    return False
+
+
+# print(palindrome('racecar'))
+# print(palindrome('Noon'))
+# print(palindrome('ciVic'))
+# print(palindrome('nice'))
+# # print(palindrome(434))
+# # print(palindrome(123))
+# print(palindrome('bomb'))
+
+# print("The following should be True if you're trying to do the extra portion of this challenge")
+# print(palindrome('Sore was I ere I saw Eros.'))
+# print(palindrome('A man, a plan, a canal -- Panama'))
